c546263a4740277f1ad9348e61c372beaca538b7
mysecondgit <hudeifa2023@gmail.com>
2024-03-14 15:08:33 +0300

    intial

---
+++ app/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="border-b border-[#3a3e41] px-44 py-4 flex justify-between items-center">
+      <span>Myui</span>
+      <button className="py-2 px-6 bg-[#3a3e41] rounded-lg text-sm">Login</button>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoginPage = () => {
+  return <div className="bg-red-200 flex justify-center">
+    
+  </div>;
+};
+
+export default LoginPage;
